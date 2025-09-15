50f8f904abcb990b32218a7b8267a7af5281eff0
Viktorbikfalvi1 <Viktor.bikfalvi@elev.ga.lbs.se>
2025-09-15 13:48:19 +0200

    rotation video 12

--- TopDownShooter-LBS25/Assets/Scripts/Player.cs
+++ TopDownShooter-LBS25/Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 moveInput;
-    [SerializeField] float moveSpeed = 4;
-    [SerializeField] float bulletSpeed = 4;
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotationSpeed = 700f;
+    [SerializeField] float bulletSpeed = 7f;
     [SerializeField] GameObject bullet;
+    [SerializeField] GameObject gun;
 
+    float targetAngle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,12 +25,24 @@ public class Player : MonoBehaviour
 
     void OnAttack()
     {
-        Rigidbody2D playerBullet = Instantiate(bullet,transform.position, Quaternion.identity).GetComponent<Rigidbody2D>(); 
+        Rigidbody2D playerBullet = Instantiate(bullet, gun.transform    .position, transform.rotation).GetComponent<Rigidbody2D>(); 
         playerBullet.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
     }
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+        if (moveInput != Vector2.zero)
+        {
+            targetAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+        }
     }
+
+    void FixedUpdate()
+    {
+       float rotation = Mathf.MoveTowardsAngle(rb.rotation, targetAngle - 90, rotationSpeed * Time.fixedDeltaTime); 
+        rb.MoveRotation(rotation);
+    }
+
+
 }
