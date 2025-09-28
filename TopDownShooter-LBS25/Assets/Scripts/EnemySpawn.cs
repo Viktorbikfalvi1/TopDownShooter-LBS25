36ab32b8a202470ea9b5a808fa01bd9ae39f72fa
Viktorbikfalvi1 <Viktor.bikfalvi@elev.ga.lbs.se>
2025-09-28 23:10:01 +0200

    spawner script start

--- TopDownShooter-LBS25/Assets/Scripts/EnemySpawn.cs
+++ TopDownShooter-LBS25/Assets/Scripts/EnemySpawn.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float minSpawnTime = 1.0f;
+    [SerializeField] float maxSpawnTime = 3.0f;
+
+    float spawnDistance = 10f;
+    Vector2 screenBounds;
+    Vector2 spawnPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
